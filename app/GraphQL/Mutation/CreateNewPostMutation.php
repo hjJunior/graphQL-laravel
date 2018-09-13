64a6357464dfcfbe07a1329bc415e5bfa9f56340
Helio S. Junior <hj_junior@hotmail.com>
2018-09-13 09:04:46 -0300

    Removed unnecessary import

--- app/GraphQL/Mutation/CreateNewPostMutation.php
+++ app/GraphQL/Mutation/CreateNewPostMutation.php
@@ -12,7 +12,6 @@ use GraphQL;
 use GraphQL\Type\Definition\Type;
 use Folklore\GraphQL\Support\Mutation;
 use App\Post;
-use App\GraphQL\Type\PostType;
 
 
 class CreateNewPostMutation extends Mutation
@@ -28,7 +27,6 @@ class CreateNewPostMutation extends Mutation
 
     public function args()
     {
-        $postType = new PostType();
         return [
             'user_id' => [
                 'type' => Type::nonNull(Type::id()),
